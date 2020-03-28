ebeef3dc9d0001c168179e560e0c3781e2d66816
MaviLima <marcos43@mail.uft.edu.br>
2020-03-28 17:31:31 -0300

    Front-end do app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const routes = require('./routes');
+const router = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
-// app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(cors());
+app.use(router);
 
 app.listen(3333);
